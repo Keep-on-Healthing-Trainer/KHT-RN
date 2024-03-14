e607a8c81f60a88e6c23682811f31bb861e307b8
downpour07 <choiheeyoung10@gmail.com>
2024-03-14 22:59:17 +0900

    📑 :: button components

---
+++ kht/src/components/Buttons/BorderBlueButton.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { TouchableOpacity, StyleSheet, Text } from "react-native";
+import { color } from "../../styles/theme"
+
+const BorderBlueButton = ({innerText, navigation}) => {
+
+  return (
+    <TouchableOpacity style={Styles.Bottom}>
+        <Text style={Styles.BottomText}>{innerText}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const Styles = StyleSheet.create({
+    Bottom: {
+        backgroundColor: color.White,
+        marginTop: "5%",
+        padding: 10,
+        width: "40%",
+        alignSelf: "center",
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: color.Blue[10]
+      },
+      BottomText: {
+        fontSize: 16,
+        color: color.Blue[10],
+        textAlign: "center",
+      }
+})
+
+export default BorderBlueButton;
