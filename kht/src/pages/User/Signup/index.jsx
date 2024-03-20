d1f18859d72f5abd611c396ab5f6b7de21dfc11f
downpour07 <choiheeyoung10@gmail.com>
2024-03-20 23:25:25 +0900

    📝 :: Signup 작성

--- kht/src/pages/User/Signup/index.jsx
+++ kht/src/pages/User/Signup/index.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
-import {Styles as S} from './styled';
+import { Styles as S } from './styled';
+import BackPage from "../../../components/header/BackPage";
+import InputText from "../../../components/Inputs/Input";
+import Button from '../../../components/Buttons/ColorBlueButton';
 
 const Signup = ({navigation}) => {
 
   return (
-    <View style={S.container}>      
-      <Text style={S.HomeText}>회원가입 화면</Text>
-      <TouchableOpacity
-          onPress={() => navigation.navigate("Rank", { screen: 'Rank' })}
-          style={S.NextBottom}
-        >
-          <Text style={S.BottomText}>랭킹 화면으로</Text>
-        </TouchableOpacity>
-    </View>
+    <>
+      <View style={S.container}>
+        <BackPage innerText="회원가입" onPress={()=>{navigation.goBack()}}></BackPage>
+        <View style={S.inputOutContainer}>
+          <View style={S.inputContainer}>
+            <Text style={S.textTitle}>이름</Text>
+            <InputText name={false}></InputText>
+          </View>
+          <View style={S.inputContainer}>
+            <Text style={S.textTitle}>아이디</Text>
+            <InputText name={false}></InputText>
+          </View>
+          <View style={S.inputContainer}>
+            <Text style={S.textTitle}>전화번호</Text>
+            <InputText name={false}></InputText>
+          </View>
+          <View style={S.inputContainer}>
+            <Text style={S.textTitle}>비밀번호</Text>
+            <InputText name={false}></InputText>
+          </View>
+          <View style={S.inputContainer}>
+            <Text style={S.textTitle}>비밀번호 확인</Text>
+            <InputText name={false}></InputText>
+          </View>
+        </View>
+        <View style={S.buttonContainer}>
+            <Button innerText="회원가입" onPress={() => navigation.navigate("Login", { screen: 'Login' })}></Button>
+        </View>
+      </View>
+    </>
   )
 }
 
--- kht/src/pages/User/Signup/styled.jsx
+++ kht/src/pages/User/Signup/styled.jsx
@@ -1,28 +1,32 @@
 import { StyleSheet } from "react-native";
 import constants from '../../../styles/constants'
+import { color } from '../../../styles/theme'
 
 export const Styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: color.White,
       width: constants.width,
-      height: constants.height
+      height: constants.height,
+      alignItems: 'center'
     },
-    HomeText: {
-      fontSize: 30,
-      textAlign: "center",
+    textTitle: {
+      backgroundColor: color.White,
+      color: color.Black,
+      fontSize: 14,
+      fontWeight: 'medium',
+      marginBottom: constants.height/70
     },
-    NextBottom: {
-      backgroundColor: "purple",
-      padding: 10,
-      marginTop: "20%",
-      width: "50%",
-      alignSelf: "center",
-      borderRadius: 10,
+    inputOutContainer: {
+      width: constants.width/10*8,
+      height: constants.height/1.7,
+      marginTop: constants.height/40
     },
-    BottomText: {
-      fontSize: 15,
-      color: 'white',
-      textAlign: "center",
+    inputContainer: {
+      width: constants.width/10*8,
+      marginTop: constants.height/80
+    },
+    buttonContainer: {
+      marginTop: constants.height/10
     }
 })
