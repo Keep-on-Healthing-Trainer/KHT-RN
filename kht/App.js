d3655402b481b32d594e4d479030cce04e6213b5
downpour07 <choiheeyoung10@gmail.com>
2024-03-20 23:25:50 +0900

    ⚙️ :: setting update

--- kht/App.js
+++ kht/App.js
@@ -7,7 +7,8 @@ import Login from './src/pages/User/Login';
 import Signup from './src/pages/User/Signup';
 import Rank from './src/pages/Rank';
 import Info from './src/pages/User/Info';
-import Loading from './src/pages/Loading'
+import Loading from './src/pages/Loading';
+import Main from './src/pages/Main';
 
 const Stack = createNativeStackNavigator();
 
@@ -32,10 +33,11 @@ export default class extends React.Component{
 
         <NavigationContainer>
           <Stack.Navigator initialRouteName='Login'>
-            <Stack.Screen options = {{ headerShown: false}} name="Login" component={Login} />
-            <Stack.Screen options = {{ headerShown: false}} name="Signup" component={Signup} />
-            <Stack.Screen options = {{ headerShown: false}} name="Rank" component={Rank} />
-            <Stack.Screen options = {{ headerShown: false}} name="Info" component={Info} />
+            <Stack.Screen options = {{ headerShown: false }} name="Login" component={Login} />
+            <Stack.Screen options = {{ headerShown: false }} name="Signup" component={Signup} />
+            <Stack.Screen options = {{ headerShown: false }} name="Rank" component={Rank} />
+            <Stack.Screen options = {{ headerShown: false }} name="Info" component={Info} />
+            <Stack.Screen options = {{ headerShown: false }} name="Main" component={Main} />
           </Stack.Navigator>
         </NavigationContainer>
         
