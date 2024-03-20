e5522ce6753439d804841a66ab163e5b0c0e18ed
downpour07 <choiheeyoung10@gmail.com>
2024-03-20 23:23:45 +0900

    📝 :: input 수정

--- kht/src/components/Inputs/Input/index.jsx
+++ kht/src/components/Inputs/Input/index.jsx
@@ -3,22 +3,23 @@ import { TextInput, StyleSheet, Text } from "react-native";
 import { color } from "../../../styles/theme"
 import constants from '../../../styles/constants'
 
-const Input = ({innerText, navigation}) => {
+const Input = ({innerText, navigation, name}) => {
 
   return (
-    <TextInput style={Styles.Bottom} placeholder={innerText} secureTextEntry={true}></TextInput>
+    <TextInput style={Styles.Bottom} placeholder={innerText} secureTextEntry={name}></TextInput>
   )
-  
 }
 
 const Styles = StyleSheet.create({
     Bottom: {
-        backgroundColor: color.Gray[0],
-        marginTop: "5%",
-        width: constants.width/10*7,
+        backgroundColor: color.Gray[1],
+        marginBottom: "5%",
+        width: constants.width/10*8,
         padding: 10,
-        alignSelf: "center",
         borderRadius: 10,
+        fontSize: 16,
+        fontWeight: 'medium',
+        color: color.Black
       }
 })
 
