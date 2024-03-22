3c733b8b6c87963af7d87363b2f6cceba02110a6
downpour07 <choiheeyoung10@gmail.com>
2024-03-22 22:47:49 +0900

    📝 :: Traning 파일 수정

--- kht/src/pages/Traning/index.jsx
+++ kht/src/pages/Traning/index.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { Styles as S } from './styeld';
 import MainHeader from "../../components/header/MainHeader";
-import MainFooter from "../../components/footer/MainFooter"
 
 const Traning = ({navigation}) => {
 
   return (
     <View style={S.container}>
       <MainHeader></MainHeader>
-      <MainFooter innerText="traning"></MainFooter>
+      <Text style={S.text}>Traning 페이지</Text>
     </View>
   )
 }
