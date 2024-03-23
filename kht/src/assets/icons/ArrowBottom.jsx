d17d28d935462ab9483b7779d75f0eafdf145e24
downpour07 <choiheeyoung10@gmail.com>
2024-03-23 15:06:20 +0900

    📑 :: icons 추가

---
+++ kht/src/assets/icons/ArrowBottom.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+const ArrowBottom = (props) => (
+<Svg width="42" height="13" viewBox="0 0 42 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M20.9999 8.27263L37.3334 0L42 2.36362L20.9999 13L0 2.36362L4.66666 0L20.9999 8.27263Z" fill="white" fill-opacity="0.7"/>
+</Svg>
+)
+
+export default ArrowBottom;
