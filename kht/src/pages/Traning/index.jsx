a9a1414c2ab9303185914296527d61d7de7ecaca
downpour07 <choiheeyoung10@gmail.com>
2024-03-23 15:06:49 +0900

    📝 :: traning 파일 작성

--- kht/src/pages/Traning/index.jsx
+++ kht/src/pages/Traning/index.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { Camera } from 'expo-camera';
 import { Styles as S } from './styeld';
 import MainHeader from "../../components/header/MainHeader";
+import ArrowBottom from "../../assets/icons/ArrowBottom";
 
 const Traning = ({navigation}) => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={S.container}>
       <MainHeader></MainHeader>
-      <Text style={S.text}>Traning 페이지</Text>
+      <View style={S.mainContainer}>
+        <View style={S.textContainer}>
+          <Text style={S.traningTypeText}>운동 방법?</Text>
+          <ArrowBottom></ArrowBottom>
+        </View>
+        <View style={S.cameraContainer}>
+          <Camera
+            style={S.camera}
+            type={Camera.Constants.Type.back}
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          />
+        </View>
+        {/* {scanned && <Text style={S.text}>QR 코드 스캔 완료!</Text>} */}
+        <Text style={S.traningTypeText}>KHT 기기 화면에 표시된{"\n"}QR을 스캔해주세요</Text>
+        <TouchableOpacity style={S.button}><Text style={S.buttonText}>SCAN</Text></TouchableOpacity>
+      </View>
     </View>
   )
 }
--- kht/src/pages/Traning/styeld.jsx
+++ kht/src/pages/Traning/styeld.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet } from "react-native";
 import constants from '../../styles/constants'
+import { color } from "../../styles/theme";
 
 export const Styles = StyleSheet.create({
     container: {
@@ -7,9 +8,52 @@ export const Styles = StyleSheet.create({
       backgroundColor: '#fff',
       width: constants.width,
       height: constants.height,
-      justifyContent: 'space-between'
     },
     text: {
-      fontSize: 50
+      fontSize: 18,
+      marginTop: 20,
+      textAlign: 'center',
+    },
+    camera: {
+      flex: 1,
+      // borderRadius: 20,
+    },
+    cameraContainer: {
+      width: constants.width/10*8,
+      height: constants.height/3,
+      borderColor: color.Blue[8],
+      // borderRadius: 20,
+      borderWidth: 5,
+    },
+    mainContainer: {
+      flex: 1,
+      backgroundColor: color.Gray[3],
+      alignItems: 'center',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      justifyContent: 'space-around',
+      paddingBottom: constants.height/15
+    },
+    textContainer: {
+      alignItems: 'center',
+    },
+    traningTypeText: {
+      fontSize: 22,
+      fontWeight: 'Black',
+      textAlign: 'center',
+      color: color.White
+    },
+    buttonText: {
+      fontSize: 18,
+      textAlign: 'center',
+      color: color.White
+    },
+    button: {
+      width: constants.width/10*8,
+      height: constants.height/14,
+      backgroundColor: color.Blue[8],
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: 10
     }
 })
