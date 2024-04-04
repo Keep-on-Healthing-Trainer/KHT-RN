79d9ef247b0897fc192401bdac37b0251e22442e
downpour07 <choiheeyoung10@gmail.com>
2024-04-04 22:06:01 +0900

    📑 :: skip header 추가

---
+++ kht/src/components/header/SkipHeader/index.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Image } from "react-native";
+import constants from '../../../styles/constants';
+import { color } from '../../../styles/theme';
+
+const SkipHeader = ({innerText, navigation, onPress}) => {
+
+  return (
+    <>
+      <View style={Styles.container}>
+        <View style={Styles.textContainer}>
+          <Text style={Styles.text}>{innerText}</Text>
+        </View>
+        <TouchableOpacity onPress={onPress}>
+            <Text style={Styles.skipButton}>건너뛰기</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  )
+}
+
+const Styles = StyleSheet.create({
+    container: {
+        backgroundColor: color.White,
+        width: constants.width,
+        height: constants.height/12,
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-around',
+        paddingLeft: 70,
+        paddingRight: 20,
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+      color: color.Black,
+      fontSize: 18,
+      fontWeight: 'medium',
+    },
+    skipButton: {
+        color: color.Blue[7],
+    }
+})
+
+export default SkipHeader;
