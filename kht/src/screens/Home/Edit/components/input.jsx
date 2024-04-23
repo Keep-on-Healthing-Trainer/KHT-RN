f4858cfa66f9571a4b25cc8325e3a3ce35811a3c
downpour07 <choiheeyoung10@gmail.com>
2024-04-23 20:11:43 +0900

    ♻️ :: HomeEdit refactoring

---
+++ kht/src/screens/Home/Edit/components/input.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import InputCheck from "../../../../components/Inputs/InputCheck";
+
+import { color } from "../../../../styles/theme";
+import constants from "../../../../styles/constants";
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from '@expo-google-fonts/roboto';
+
+const Input = ({route, navigation}) => {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+    
+  const {name} = route.params;
+
+  const getValue=(onGetInText)=>{
+    console.log('이름 : ' + onGetInText);
+    setData(onGetInText);
+  }
+
+  return (
+    <View style={Styles.titleContainer}>
+        <Text style={Styles.nameTitle}>{name}</Text>
+        <InputCheck
+        innerText="이나경" 
+        onGetInText={getValue}
+        />
+    </View>
+  )
+}
+
+const Styles = StyleSheet.create({
+    titleContainer: {
+      marginTop: constants.height/30,
+      marginBottom: constants.height/2
+    },
+    nameTitle: {
+      color: color.Gray[4],
+      fontSize: 12,
+      marginLeft: constants.width/40,
+      fontFamily: "Roboto_400Regular"
+    }
+})
+
+export default Input;
--- kht/src/screens/Home/Edit/index.jsx
+++ kht/src/screens/Home/Edit/index.jsx
@@ -1,33 +1,33 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-import {Styles as S} from './styled';
+import { color } from "../../../styles/theme";
+import constants from "../../../styles/constants";
+
 import BackPage from "../../../components/header/BackHeader";
-import InputCheck from "../../../components/Inputs/InputCheck";
 import Button from "../../../components/Buttons/BorderBlueButton";
+import Input from "./components/input";
 
 const EditNamTab = ({route, navigation}) => {
   const {name} = route.params;
-  const [data, setData] = useState('');
-
-  const getValue=(onGetInText)=>{
-    console.log('이름 : ' + onGetInText);
-    setData(onGetInText);
-   }
 
   return (
-    <View style={S.container}>
-        <BackPage innerText={name} onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })}></BackPage>
-        <View style={S.titleContainer}>
-          <Text style={S.nameTitle}>{name}</Text>
-          <InputCheck
-          innerText="이나경" 
-          onGetInText={getValue}
-          ></InputCheck>
-        </View>
-        <Button innerText="저장하기" onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })}></Button>
+    <View style={Styles.container}>
+        <BackPage innerText={name} onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })} />
+        <Input />
+        <Button innerText="저장하기" onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })} />
     </View>
   )
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: color.White,
+    width: constants.width,
+    height: constants.height,
+    alignItems: 'center'
+  }
+})
 
 export default EditNamTab;
