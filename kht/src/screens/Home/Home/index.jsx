ac36924a7f3f290cb1fa846dcf754788b137bc3e
downpour07 <choiheeyoung10@gmail.com>
2024-04-24 08:31:24 +0900

    ♻️ :: Home refactoring

--- kht/src/screens/Home/Home/index.jsx
+++ kht/src/screens/Home/Home/index.jsx
@@ -1,41 +1,141 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-import {Styles as S} from './styled';
 import MainHeader from "../../../components/header/MainHeader";
 import Profile from "../../../assets/icons/Profile";
 import Chart from "./LineChart";
 
+import constants from "../../../styles/constants";
+import { color } from "../../../styles/theme";
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  Roboto_900Black,
+} from '@expo-google-fonts/roboto';
+
 const HomeTab = ({navigation}) => {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black,
+  });
+
   const [profileLink, setProfileLink] = useState(null);
 
   return (
-    <View style={S.container}>
+    <View style={Styles.container}>
       <MainHeader></MainHeader>
-      <View style={S.topContainer}>
-        <View style={S.profileMargin}></View>
-        <View style={S.profileContainer}>
-          <TouchableOpacity style={S.editButton} onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })}>
-            <Text style={S.editButtonText}>편집</Text>
+      <View style={Styles.topContainer}>
+        <View style={Styles.profileMargin}></View>
+        <View style={Styles.profileContainer}>
+          <TouchableOpacity style={Styles.editButton} onPress={() => navigation.navigate("SelectTab", { screen: 'SelectTab' })}>
+            <Text style={Styles.editButtonText}>편집</Text>
           </TouchableOpacity>
-          <View style={S.nameContanier}>
-            <Text style={S.IdName}>이나경</Text>
-            <Text style={S.IdComment}>누적 윗몸말아올리기 횟수 : 1003</Text>
+          <View style={Styles.nameContanier}>
+            <Text style={Styles.IdName}>이나경</Text>
+            <Text style={Styles.IdComment}>누적 윗몸말아올리기 횟수 : 1003</Text>
           </View>
         </View>
         {profileLink ? (
           <></>
         ) : (
-          <Profile style={S.profile}></Profile>
+          <Profile style={Styles.profile}></Profile>
         )}
       </View>
-      <View style={S.dataContainer}>
-        <Text style={S.textCount}>총 1003회</Text>
-        <Text style={S.textComment}>KHT와 함께 1003회 윗몸일으키기를 진행했어요</Text>
+      <View style={Styles.dataContainer}>
+        <Text style={Styles.textCount}>총 1003회</Text>
+        <Text style={Styles.textComment}>KHT와 함께 1003회 윗몸일으키기를 진행했어요</Text>
         <Chart></Chart>
       </View>
   </View>
   )
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: color.Gray[1],
+    width: constants.width,
+    height: constants.height,
+  },
+  topContainer: {
+    width: constants.width,
+    height: constants.height/4,
+    alignItems: 'center',
+    position: 'relative',
+    marginTop: constants.height/20
+  },
+  profileMargin: {
+    backgroundColor: color.Gray[1],
+    width: constants.width,
+    height: constants.height/20,
+  },
+  profile: {
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+    position: 'absolute',
+  },
+  profileContainer: {
+    width: constants.width,
+    height: constants.height/5.5,
+    backgroundColor: color.White,
+    borderRadius: 20,
+    alignItems: 'flex-end',
+  },
+  dataContainer: {
+    width: constants.width,
+    height: constants.height/2.5,
+    backgroundColor: color.White,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  editButton: {
+    width: constants.width/8,
+    height: constants.height/40,
+    borderRadius: 20,
+    backgroundColor: color.Gray[3],
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: constants.width/20
+  },
+  editButtonText: {
+    color: color.White
+  },
+  textCount: {
+    color: color.Black,
+    fontSize: 20,
+    fontFamily: "Roboto_400Regular"
+  },
+  textComment: {
+    color: color.Black,
+    fontSize: 12,
+    marginBottom: 50,
+    fontFamily: "Roboto_400Regular"
+  },
+  nameContanier: {
+    width: constants.width,
+    alignItems: 'center'
+  },
+  IdName: {
+    fontSize: 20,
+    fontFamily: "Roboto_400Regular",
+    color: color.Gray[5],
+    marginBottom: 10
+  },
+  IdComment: {
+    fontSize: 14,
+    fontFamily: "Roboto_400Regular",
+    color: color.Gray[4]
+  }
+})
 
 export default HomeTab;
