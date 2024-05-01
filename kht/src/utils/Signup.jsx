5a0fab2441e2082acb9c8a3bd2e9860ec1b4fd8f
downpour07 <choiheeyoung10@gmail.com>
2024-05-01 23:41:53 +0900

    ⚡️ :: Signup API 연동

--- kht/src/utils/Signup.jsx
+++ kht/src/utils/Signup.jsx
@@ -2,18 +2,19 @@ import axios from 'axios';
 import { API_KEY } from "@env";
 
 const onSignup = async ( data ) => {
-    // try {
-    //     const response = await axios.post(`${API_KEY}/user/signup`, {
-    //         "userId" : data.userId,
-    //         "password" : data.password,
-    //         "userName" : data.userName,
-    //         "phoneNumber" : data.phoneNumber
-    //     });
-    //     console.log("받은 값:", response);
-    //     console.log("회원가입에 성공하였습니다.");
-    // } catch (error) {
-    //     console.error("회원가입 ERROR :", error);
-    // }
+    try {
+        const response = await axios.post(`${API_KEY}/user/signup`, {
+            "userId" : data.userId,
+            "name" : data.name,
+            "phoneNumber" : data.phoneNumber,
+            "password" : data.password
+        });
+        if(response.status == 201) {
+            console.log("회원가입에 성공하였습니다.");
+        }
+    } catch (error) {
+        console.error("회원가입 ERROR :", error);
+    }
 };
 
 export default onSignup;
