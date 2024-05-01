dc06d557c80bd79495a750e400f5fcc65a32fb84
downpour07 <choiheeyoung10@gmail.com>
2024-05-01 23:41:42 +0900

    ⚡️ :: User API 연동

--- kht/src/utils/User.jsx
+++ kht/src/utils/User.jsx
@@ -2,19 +2,18 @@ import axios from 'axios';
 import { API_KEY } from "@env";
 
 const onUser = async () => {
-    // const accessToken = '';
+    const token = "";
 
-    // try {
-    //     const response = await axios.get(`${API_KEY}/user`, {
-    //         headers: {
-    //             Authorization: `Bearer ${accessToken}`
-    //         }
-    //     });
-    //     console.log("받은 값:", response);
-    //     console.log("유저 조회에 성공하였습니다.");
-    // } catch (error) {
-    //     console.error("유저 조회 ERROR :", error);
-    // }
+    try {
+        const response = await axios.get(`${API_KEY}/user`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error("유저 정보 조회 ERROR :", error);
+    }
 };
 
 export default onUser;
