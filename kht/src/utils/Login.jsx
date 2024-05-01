ec5b1c930a698870a131d6329a71b36e397b0a32
downpour07 <choiheeyoung10@gmail.com>
2024-05-01 23:41:30 +0900

    ⚡️ :: Login API 연동

--- kht/src/utils/Login.jsx
+++ kht/src/utils/Login.jsx
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { API_KEY } from "@env";
 
 const onLogin = async ( data, state ) => {
-    // try {
-    //     const response = await axios.post(`${API_KEY}/user/login`, {
-    //         userId: data.userId,
-    //         password: data.password
-    //     });
-    //     console.log("받은 값:", response);
-    //     console.log("로그인에 성공하였습니다.");
-    // } catch (error) {
-    //     console.error("로그인 ERROR :", error);
-    // }
+    try {
+        const response = await axios.post(`${API_KEY}/user/login`, {
+            userId: data.userId,
+            password: data.password
+        });
+        const accessToken = response.data.accessToken;
+        const refreshToken = response.data.refreshToken;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 export default onLogin;
